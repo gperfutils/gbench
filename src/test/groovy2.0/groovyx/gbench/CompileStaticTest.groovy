57447c355e3c4306a3f9f3c2d5d327ab2ffab2ce
Masato Nagai <me@masatonagai.com>
2012-07-03 02:24:42 +0900

    add a test that uses @CompileStatic

---
+++ src/test/groovy2.0/groovyx/gbench/CompileStaticTest.groovy
@@ -0,0 +1,18 @@
+package groovyx.gbench;
+
+import groovy.transform.CompileStatic;
+
+import org.junit.Test;
+
+public class CompileStaticTest {
+    
+    @CompileStatic
+    @Benchmark
+    int compiledMethod() {
+        return 0;
+    }
+    
+    @Test public void testStaticCompiledMethod() throws Exception {
+        compiledMethod();
+    }
+}
